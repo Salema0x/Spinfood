01c8c4856083b9f610223bcbb1f7315b539ba504
Bad-Buddha <dav.klitsch@gmail.com>
2023-04-23 12:54:01 +0200

    created a readCSV in main-class

---
+++ Spinfood-Projekt/src/main/java/Entity/Participant.java
@@ -0,0 +1,128 @@
+package Entity;
+
+public class Participant {
+    private String id;
+    private String name;
+    private String foodPreference;
+    private int age;
+    private String sex;
+    private String kitchen;
+    private String kitchenStory;
+    private double kitchenLongitude;
+    private double kitchenLatitude;
+    private String id2;
+    private String name2;
+    private int age2;
+    private String sex2;
+
+    // Konstruktor
+    public Participant(){
+
+    }
+
+    // Getter und Setter-Methoden
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getFoodPreference() {
+        return foodPreference;
+    }
+
+    public void setFoodPreference(String foodPreference) {
+        this.foodPreference = foodPreference;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public String getKitchen() {
+        return kitchen;
+    }
+
+    public void setKitchen(String kitchen) {
+        this.kitchen = kitchen;
+    }
+
+    public String getKitchenStory() {
+        return kitchenStory;
+    }
+
+    public void setKitchenStory(String kitchenStory) {
+        this.kitchenStory = kitchenStory;
+    }
+
+    public double getKitchenLongitude() {
+        return kitchenLongitude;
+    }
+
+    public void setKitchenLongitude(double kitchenLongitude) {
+        this.kitchenLongitude = kitchenLongitude;
+    }
+
+    public double getKitchenLatitude() {
+        return kitchenLatitude;
+    }
+
+    public void setKitchenLatitude(double kitchenLatitude) {
+        this.kitchenLatitude = kitchenLatitude;
+    }
+
+    public String getId2() {
+        return id2;
+    }
+
+    public void setId2(String id2) {
+        this.id2 = id2;
+    }
+
+    public String getName2() {
+        return name2;
+    }
+
+    public void setName2(String name2) {
+        this.name2 = name2;
+    }
+
+    public int getAge2() {
+        return age2;
+    }
+
+    public void setAge2(int age2) {
+        this.age2 = age2;
+    }
+
+    public String getSex2() {
+        return sex2;
+    }
+
+    public void setSex2(String sex2) {
+        this.sex2 = sex2;
+    }
+}
+
--- Spinfood-Projekt/src/main/java/Main.java
+++ Spinfood-Projekt/src/main/java/Main.java
@@ -1,6 +1,77 @@
+import Entity.Participant;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         //entry point
-        //test
+
+        String fileName = "teilnehmerliste.csv"; // Name der CSV-Datei
+
+        // Aufruf der readCSV-Funktion
+        try {
+            HashMap<String, Participant> participants = readCSV(fileName);
+            //Ausgabe der HashMap mit den Personen auf der Konsole (TODO)
+            printHashMap(participants);
+        } catch (URISyntaxException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static HashMap<String, Participant> readCSV(String fileName) throws URISyntaxException {
+        // Hash-Tabelle von Participants
+        HashMap<String, Participant> participants = new HashMap<String, Participant>();
+
+        URL resource = Main.class.getResource(fileName);
+
+        // Datei öffnen
+        File file = Paths.get(resource.toURI()).toFile();
+        try {
+            Scanner inputStream = new Scanner(file);
+            inputStream.nextLine(); // Erste Zeile überspringen (Spaltenüberschriften)
+
+            // Zeilenweises Lesen der Datei
+            while(inputStream.hasNext()){
+                String data = inputStream.nextLine();
+                String[] values = data.split(","); // Trennzeichen ist ","
+                // Erstellen eines neuen Participant-Objekts
+                Participant participant = new Participant();
+                participant.setId(values[1]);
+                participant.setName(values[2]);
+                participant.setFoodPreference(values[3]);
+                participant.setAge(Integer.parseInt(values[4]));
+                participant.setSex(values[5]);
+                participant.setKitchen(values[6]);
+                participant.setKitchenStory(values[7]);
+                participant.setKitchenLongitude(Double.parseDouble(values[8]));
+                participant.setKitchenLatitude(Double.parseDouble(values[9]));
+                //Manche Werte der CSV sind nicht bei jedem Participant gefüllt, weshalb/wie damit umgehen?
+                participant.setId2(values[10]);
+                participant.setName2(values[11]);
+                participant.setAge2(Integer.parseInt(values[12]));
+                participant.setSex2(values[13]);
+
+                // Participant zur Hash-Tabelle hinzufügen
+                participants.put(participant.getId(), participant);
+            }
+
+            // Datei schließen
+            inputStream.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        // Rückgabe der Hash-Tabelle von Participants
+        return participants;
+    }
+
+    public static void printHashMap(HashMap hashMap){
+        //Schöner Konsolen-Output
     }
 }
