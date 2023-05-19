f2afd4c0f26f149f3e154cc2f25b556c780bb0f5
Krell <Krell@students.uni-marburg.de>
2023-05-19 15:39:51 +0200

    fixed big bug in pair algorithm, in versions before meat people were also paired with veggies, vegans

--- Spinfood-Projekt/src/main/java/Factory/PairListFactory.java
+++ Spinfood-Projekt/src/main/java/Factory/PairListFactory.java
@@ -6,6 +6,7 @@ import Entity.Participant;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class PairListFactory {
     public List<Pair> registeredPairs;
@@ -24,18 +25,30 @@ public class PairListFactory {
 
         yesKitchenParticipants.add(createList("yes", "none"));
         yesKitchenParticipants.add(createList("yes", "meat"));
-        yesKitchenParticipants.add(createList("yes", "veggie"));
-        yesKitchenParticipants.add(createList("yes", "vegan"));
+
+        List<Participant> veggieList = createList("yes", "veggie");
+        List<Participant> veganList = createList("yes", "vegan");
+        List<Participant> veggieVeganList = Stream.concat(veggieList.stream(), veganList.stream()).toList();
+
+        yesKitchenParticipants.add(veggieVeganList);
 
         maybeKitchenParticipants.add(createList("maybe", "none"));
         maybeKitchenParticipants.add(createList("maybe", "meat"));
-        maybeKitchenParticipants.add(createList("maybe", "veggie"));
-        maybeKitchenParticipants.add(createList("maybe", "vegan"));
+
+        veggieList = createList("maybe", "veggie");
+        veganList = createList("maybe", "vegan");
+        veggieVeganList = Stream.concat(veggieList.stream(), veganList.stream()).toList();
+
+        maybeKitchenParticipants.add(veggieVeganList);
 
         noKitchenParticipants.add(createList("no", "none"));
         noKitchenParticipants.add(createList("no", "meat"));
-        noKitchenParticipants.add(createList("no", "veggie"));
-        noKitchenParticipants.add(createList("no", "vegan"));
+
+        veggieList = createList("no", "veggie");
+        veganList = createList("no", "vegan");
+        veggieVeganList = Stream.concat(veggieList.stream(), veganList.stream()).toList();
+
+        noKitchenParticipants.add(veggieVeganList);
 
         decideAlgorithm();
         makePairs();
@@ -189,6 +202,9 @@ public class PairListFactory {
 
     private void makePairs() {
 
+        makePairsMeat();
+        makePairsOther();
+
         /*
         if (!yesKitchenParticipants.isEmpty() && !noKitchenParticipants.isEmpty()) {
             while (!yesKitchenParticipants.isEmpty() && !noKitchenParticipants.isEmpty()) {
@@ -222,21 +238,76 @@ public class PairListFactory {
             }
         } */
 
-        identifySuccessors();
     }
 
-    private void split() {
+    private void makePairsMeat() {
+        List<Participant> noneParticipantsYesKitchen = yesKitchenParticipants.get(0);
+        List<Participant> meatParticipantsYesKitchen = yesKitchenParticipants.get(1);
+        List<Participant> noneParticipantsNoKitchen = noKitchenParticipants.get(0);
+        List<Participant> meatParticipantsNoKitchen = noKitchenParticipants.get(1);
+        List<Participant> noneParticipantsMaybeKitchen = maybeKitchenParticipants.get(0);
+        List<Participant> meatParticipantsMaybeKitchen = maybeKitchenParticipants.get(1);
 
+        makePairsStarter(meatParticipantsYesKitchen, meatParticipantsMaybeKitchen, meatParticipantsNoKitchen);
+
+        /*
+            List<List<Participant>> noneParticipants = new ArrayList<>(List.of(noneParticipantsYesKitchen, noneParticipantsNoKitchen, noneParticipantsMaybeKitchen));
+            List<List<Participant>> meatParticipants = new ArrayList<>(List.of(meatParticipantsYesKitchen, meatParticipantsNoKitchen, meatParticipantsMaybeKitchen));
+
+            identifySuccessors();
+         */
     }
 
-    private void identifySuccessors() {
-        if (yesKitchenParticipants.size() == 1) {
-           // yesKitchenParticipants.get(0).setSuccessor(true);
-        }
+    private void makePairsOther() {
+        List<Participant> noneParticipantsYesKitchen = yesKitchenParticipants.get(0);
+        List<Participant> noneParticipantsMaybeKitchen = maybeKitchenParticipants.get(0);
+        List<Participant> noneParticipantsNoKitchen = noKitchenParticipants.get(0);
+        List<Participant> otherParticipantsYesKitchen = yesKitchenParticipants.get(2);
+        List<Participant> otherParticipantsMaybeKitchen = maybeKitchenParticipants.get(2);
+        List<Participant> otherParticipantsNoKitchen = noKitchenParticipants.get(2);
 
-        if (maybeKitchenParticipants.size() == 1) {
-           // maybeKitchenParticipants.get(0).setSuccessor(true);
+        makePairsStarter(otherParticipantsYesKitchen, otherParticipantsMaybeKitchen, otherParticipantsNoKitchen);
+
+        List<List<Participant>> noneParticipants = new ArrayList<>(List.of(noneParticipantsYesKitchen, noneParticipantsNoKitchen, noneParticipantsMaybeKitchen));
+        List<List<Participant>> otherParticipants = new ArrayList<>(List.of(otherParticipantsYesKitchen, otherParticipantsNoKitchen, otherParticipantsMaybeKitchen));
+
+        identifySuccessors(noneParticipants, otherParticipants);
+    }
+
+    private void makePairsStarter(List<Participant> yesKitchen, List<Participant> maybeKitchen, List<Participant> noKitchen) {
+        makePairs(yesKitchen, noKitchen);
+        makePairs(yesKitchen, maybeKitchen);
+        makePairs(yesKitchen);
+        makePairs(maybeKitchen, noKitchen);
+        makePairs(maybeKitchen);
+    }
+
+    @SafeVarargs
+    private void makePairs(List<Participant> participantList1, List<Participant>... optionalParticipantList) {
+        List<Participant> participantList2;
+        if (optionalParticipantList.length > 0) {
+            participantList2 = optionalParticipantList[0];
+
+            if (!participantList1.isEmpty() && !participantList2.isEmpty()) {
+                while (!participantList1.isEmpty() && !participantList2.isEmpty()) {
+                    Participant participant1 = participantList1.remove(0);
+                    Participant participant2 = participantList2.remove(0);
+                    pairList.add(new Pair(participant1, participant2));
+                }
+            }
+        } else {
+            if (!participantList1.isEmpty()) {
+                while (participantList1.size() > 1) {
+                    Participant participant1 = participantList1.remove(0);
+                    Participant participant2 = participantList1.remove(0);
+                    pairList.add(new Pair(participant1, participant2));
+                }
+            }
         }
+    }
+
+    private void identifySuccessors(List<List<Participant>> noneParticipants, List<List<Participant>> restParticipants) {
+
 
     }
 
