a8c15fcf001d8c67da090a0b3e539f95d40e7376
Scheil <Scheil@students.uni-marburg.de>
2023-05-20 13:20:12 +0200

    Testframework updated + new Data

--- Spinfood-Projekt/src/test/java/Factory/PairListFactoryTest.java
+++ Spinfood-Projekt/src/test/java/Factory/PairListFactoryTest.java
@@ -2,9 +2,13 @@ package Factory;
 
 import Entity.Pair;
 import Entity.Participant;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.lang.reflect.Array;
 import java.net.URISyntaxException;
 import java.util.ArrayList;
@@ -12,85 +16,104 @@ import java.util.List;
 import java.util.Objects;
 
 class PairListFactoryTest {
+
     PairListFactory pairListFactory;
     ParticipantFactory participantFactory;
 
-    List<Participant> participantList1;
-    List<Participant> participantList2;
-    List<Pair> pairList = new ArrayList<>() {{
-        add(new Pair)
-    }};
-    List<File> testLists = new ArrayList<>() {{
-        try {
-            add(new File(Objects.requireNonNull(getClass().getResource("/testlists/pairfactorytestlists/testliste0.csv")).toURI()));
-            add(new File(Objects.requireNonNull(getClass().getResource("/testlists/pairfactorytestlists/testliste1.csv")).toURI()));
-            add(new File(Objects.requireNonNull(getClass().getResource("/testlists/pairfactorytestlists/testliste2.csv")).toURI()));
-            add(new File(Objects.requireNonNull(getClass().getResource("/testlists/pairfactorytestlists/testliste3.csv")).toURI()));
-            add(new File(Objects.requireNonNull(getClass().getResource("/testlists/pairfactorytestlists/testliste4.csv")).toURI()));
-            add(new File(Objects.requireNonNull(getClass().getResource("/testlists/pairfactorytestlists/testliste5.csv")).toURI()));
-        } catch (URISyntaxException e) {
-            throw new RuntimeException(e);
-        }
-    }};
-    List<List<Integer>> criteriaOrder = new ArrayList<>() {{
-        add(criteriaOrder1);
-        add(criteriaOrder2);
-        add(criteriaOrder3);
-        add(criteriaOrder4);
-        add(criteriaOrder5);
-        add(criteriaOrder6);
-    }};
-    List<Integer> criteriaOrder1 = new ArrayList<>() {{
-        add(5);
-        add(6);
-        add(7);
-    }};
-    List<Integer> criteriaOrder2 = new ArrayList<>() {{
-        add(6);
-        add(5);
-        add(7);
-    }};
-    List<Integer> criteriaOrder3 = new ArrayList<>() {{
-        add(7);
-        add(6);
-        add(5);
-    }};
-    List<Integer> criteriaOrder4 = new ArrayList<>() {{
-        add(5);
-        add(7);
-        add(6);
-    }};
-    List<Integer> criteriaOrder5 = new ArrayList<>() {{
-        add(6);
-        add(7);
-        add(5);
-    }};
-    List<Integer> criteriaOrder6 = new ArrayList<>() {{
-        add(7);
-        add(5);
-        add(6);
-    }};
+    List<Participant> participantsList; //List of participants
+
+    List<List<Pair>> expectedPairLists; //List of expected PairLists
+
+    List<List<Object>> criteriaOrder; //List of criteria orders
 
 
 
     @BeforeEach
-    void setUp() throws URISyntaxException {
+    void setUp() {
         participantFactory = new ParticipantFactory();
-        participantFactory.readCSV(new File(Objects.requireNonNull(getClass().getResource("/testlists/pairfactorytestlist/testliste0.csv")).toURI()));
-        participantList2 = participantFactory.readCSV(new File(Objects.requireNonNull(getClass().getResource("/testlists/pairfactorytestlist/testliste1.csv")).toURI()));
+
     }
+
+
 
     @org.junit.jupiter.api.Test
     void makePairs() throws URISyntaxException {
-        //testing if pairs are correctly matched in criteria Orders 1-7
-        for (List<Integer> criteria : criteriaOrder) {
 
-        }
-             ) {
-
-        }
         participantFactory.readCSV(new File(Objects.requireNonNull(getClass().getResource("/testlists/pairfactorytestlists/testliste0.csv")).toURI()));
-        pairListFactory.makePairs(participantFactory.getParticipantList(), participantFactory.getRegisteredPairs(), );
+        participantsList = participantFactory.getParticipantList();
+        initializeCriteria();
+        initializeExpectedPairLists();
+        List<Object> criteria = new ArrayList<>();
+        List<Pair> expectedPairs = new ArrayList<>();
+
+
+        //testing if pairs are correctly matched in criteria Orders 1-7
+        for (Criteria c : Criteria.values()) {
+
+            switch (c) {
+                case CRITERIA_567:
+                    criteria = criteriaOrder.get(0);
+                    expectedPairs = expectedPairLists.get(0);
+                    break;
+                case CRITERIA_576:
+                    criteria = criteriaOrder.get(1);
+                    expectedPairs = expectedPairLists.get(1);
+                    break;
+                case CRITERIA_657:
+                    criteria = criteriaOrder.get(2);
+                    expectedPairs = expectedPairLists.get(2);
+                    break;
+                case CRITERIA_675:
+                    criteria = criteriaOrder.get(3);
+                    expectedPairs = expectedPairLists.get(3);
+                    break;
+                case CRITERIA_756:
+                    criteria = criteriaOrder.get(4);
+                    expectedPairs = expectedPairLists.get(4);
+                    break;
+                case CRITERIA_765:
+                    criteria = criteriaOrder.get(5);
+                    expectedPairs = expectedPairLists.get(5);
+                    break;
+            }
+            pairListFactory = new PairListFactory(participantFactory.getParticipantList(), participantFactory.getRegisteredPairs(), criteria);
+            pairListFactory.makePairs();
+            List<Pair> actualPairs = pairListFactory.pairList;
+
+
+            Assertions.assertEquals(expectedPairs, actualPairs);
+
+        }
     }
+
+    //Enum for criteria orders
+    //5 = foodPreference, 6 = Altersdifferenz, 7 = Geschlecht
+    private enum Criteria {
+        CRITERIA_567,       //foodPreference > Altersdifferenz > Geschlecht
+        CRITERIA_576,
+        CRITERIA_657,
+        CRITERIA_675,
+        CRITERIA_756,
+        CRITERIA_765,
+    }
+    void initializeExpectedPairLists() {
+        expectedPairLists.add(new ArrayList<>() {{add(new Pair(participantsList.get(0), participantsList.get(1))); add(new Pair(participantsList.get(2), participantsList.get(3))); add(new Pair(participantsList.get(4), participantsList.get(5)));}});
+        expectedPairLists.add(new ArrayList<>() {{add(new Pair(participantsList.get(0), participantsList.get(1))); add(new Pair(participantsList.get(2), participantsList.get(3))); add(new Pair(participantsList.get(4), participantsList.get(5)));}});
+        expectedPairLists.add(new ArrayList<>() {{add(new Pair(participantsList.get(0), participantsList.get(1))); add(new Pair(participantsList.get(2), participantsList.get(3))); add(new Pair(participantsList.get(4), participantsList.get(5)));}});
+        expectedPairLists.add(new ArrayList<>() {{add(new Pair(participantsList.get(0), participantsList.get(1))); add(new Pair(participantsList.get(2), participantsList.get(3))); add(new Pair(participantsList.get(4), participantsList.get(5)));}});
+        expectedPairLists.add(new ArrayList<>() {{add(new Pair(participantsList.get(0), participantsList.get(1))); add(new Pair(participantsList.get(2), participantsList.get(3))); add(new Pair(participantsList.get(4), participantsList.get(5)));}});
+        expectedPairLists.add(new ArrayList<>() {{add(new Pair(participantsList.get(0), participantsList.get(1))); add(new Pair(participantsList.get(2), participantsList.get(3))); add(new Pair(participantsList.get(4), participantsList.get(5)));}});
+    }
+
+    void initializeCriteria() {
+        criteriaOrder = new ArrayList<>();
+        criteriaOrder.add(new ArrayList<>() {{add(5); add(6); add(7);}});
+        criteriaOrder.add(new ArrayList<>() {{add(5); add(7); add(6);}});
+        criteriaOrder.add(new ArrayList<>() {{add(6); add(5); add(7);}});
+        criteriaOrder.add(new ArrayList<>() {{add(6); add(7); add(5);}});
+        criteriaOrder.add(new ArrayList<>() {{add(7); add(5); add(6);}});
+        criteriaOrder.add(new ArrayList<>() {{add(7); add(6); add(5);}});
+    }
+
 
 }
