3a3cb470eb33f2c52fd086991c0915c76bf5dbb5
Salema0x <saleminfo50@gmail.com>
2023-07-01 12:57:55 +0100

    Added more keywords in properties

--- Spinfood-Projekt/src/main/java/GUI/MainWindow.java
+++ Spinfood-Projekt/src/main/java/GUI/MainWindow.java
@@ -8,6 +8,7 @@ import Entity.Participant;
 import Factory.Group.GroupFactory;
 import Factory.PairListFactory;
 import Factory.ParticipantFactory;
+
 import javax.swing.*;
 import javax.swing.filechooser.FileNameExtensionFilter;
 import javax.swing.table.DefaultTableModel;
@@ -15,24 +16,26 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
-import java.util.ArrayList;
+import java.util.*;
 import java.util.List;
-import java.util.Locale;
-import java.util.ResourceBundle;
 
+/**
+ * The main window of the application.
+ */
 public class MainWindow implements ActionListener {
 
     private static final int WIDTH = 600;
     private static final int HEIGHT = 500;
-    private static final JFrame FRAME = new JFrame("Spinfood-Projekt");
-    private static final JMenuItem SHOW_PARTICIPANTS = new JMenuItem("Teilnehmerliste anzeigen");
-    private static final JMenuItem SET_CRITERIA = new JMenuItem("Wichtigkeit der Kriterien");
-    private static final JMenuItem START_PAIRS = new JMenuItem("Paare bilden");
-    private static final JMenuItem START_GROUPS = new JMenuItem("Gruppen bilden");
+    private static final JFrame FRAME = new JFrame();
+    private static final JMenuItem SHOW_PARTICIPANTS = new JMenuItem();
+    private static final JMenuItem SET_CRITERIA = new JMenuItem();
+    private static final JMenuItem START_PAIRS = new JMenuItem();
+    private static final JMenuItem START_GROUPS = new JMenuItem();
+    private static final JMenuItem ADJUST_PAIRS = new JMenuItem();
     private static final ParticipantFactory PARTICIPANT_FACTORY = new ParticipantFactory(1000);
     private static PairListFactory pairListFactory;
     private static final JLabel SHOW_TEXT = new JLabel(
-            "Starten Sie indem Sie unter 'Start' den Punkt 'Teilnehmer einlesen' auswählen.");
+            );
     private static List<Object> CRITERIA_ORDER = new ArrayList<>();
     private static final CriteriaArranger CRITERIA_WINDOW = new CriteriaArranger();
     private static boolean participantsRead = false;
@@ -47,13 +50,16 @@ public class MainWindow implements ActionListener {
     private static final JMenu languageMenu = new JMenu(("languageMenu"));
     private static final JMenu algorithmMenu = new JMenu(("algorithmMenu"));
     private static final JMenu startMenu = new JMenu(("startMenu"));
-
+    private List<Pair> pairList;
+    private Stack<List<Pair>> undoStack;
+    private Stack<List<Pair>> redoStack;
 
     /**
-     * Will create a Main Window for the application using JFrame.
+     * Displays the main window of the application.
      */
     public void displayWindow() {
         loadLanguageResources("de");
+        initializePairList();
         FRAME.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         FRAME.pack();
         FRAME.setMinimumSize(new Dimension(WIDTH, HEIGHT));
@@ -62,6 +68,109 @@ public class MainWindow implements ActionListener {
         FRAME.getContentPane().add(SHOW_TEXT, BorderLayout.SOUTH);
         FRAME.setVisible(true);
         FRAME.setLocationRelativeTo(null);
+    }
+
+    /**
+     * Initializes the pair list and undo/redo stacks.
+     */
+    private void initializePairList() {
+        pairList = new ArrayList<>();
+        undoStack = new Stack<>();
+        redoStack = new Stack<>();
+    }
+
+    /**
+     * Adds a new pair to the pair list.
+     *
+     * @param pair the pair to be added
+     */
+    private void addPairToList(Pair pair) {
+        pairList.add(pair);
+        undoStack.push(new ArrayList<>(pairList)); // Save current pair list for undo
+        redoStack.clear(); // Clear redo stack when a new change is made
+        updatePairTable();
+    }
+
+    /**
+     * Removes a pair from the pair list.
+     *
+     * @param pair the pair to be removed
+     */
+    private void removePairFromList(Pair pair) {
+        pairList.remove(pair);
+        undoStack.push(new ArrayList<>(pairList)); // Save current pair list for undo
+        redoStack.clear(); // Clear redo stack when a new change is made
+        updatePairTable();
+    }
+
+    /**
+     * Undoes the last change made to the pair list.
+     */
+    private void undoLastChange() {
+        if (!undoStack.isEmpty()) {
+            redoStack.push(new ArrayList<>(pairList)); // Save current pair list for redo
+            pairList = undoStack.pop(); // Restore previous pair list
+            updatePairTable();
+        }
+    }
+
+    /**
+     * Redoes the last undone change to the pair list.
+     */
+    private void redoLastChange() {
+        if (!redoStack.isEmpty()) {
+            undoStack.push(new ArrayList<>(pairList)); // Save current pair list for undo
+            pairList = redoStack.pop(); // Restore previous pair list
+            updatePairTable();
+        }
+    }
+    private void updatePairTable() {
+        DefaultTableModel model = new DefaultTableModel();
+        JTable table = new JTable(model);
+
+        model.addColumn("Pair Nr.");
+        model.addColumn("Participant 1");
+        model.addColumn("Participant 2");
+        model.addColumn("ID 1");
+        model.addColumn("ID 2");
+        model.addColumn("Food Preference");
+        model.addColumn("Gender Diversity Score");
+        model.addColumn("Preference Deviation");
+
+        int pairInt = 0;
+
+        for (Pair pair : pairList) {
+            model.addRow(new Object[]{
+                    pairInt,
+                    pair.getParticipant1().getName(),
+                    pair.getParticipant2().getName(),
+                    pair.getParticipant1().getId(),
+                    pair.getParticipant2().getId(),
+                    pair.getFoodPreference(),
+                    pair.getGenderDiversityScore(),
+                    pair.getPreferenceDeviation(),
+                    pairInt++
+            });
+        }
+
+        JFrame frame = new JFrame("Pairs Table");
+        frame.setLayout(new BorderLayout());
+        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+
+        JScrollPane tableScrollPane = new JScrollPane(table);
+        frame.add(tableScrollPane, BorderLayout.CENTER);
+
+        JPanel southPanel = new JPanel();
+        southPanel.setLayout(new FlowLayout());
+
+        JLabel labelPairs = new JLabel("Pairs Count: " + pairList.size());
+
+        southPanel.add(labelPairs);
+
+        frame.add(southPanel, BorderLayout.SOUTH);
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
     }
 
     /**
@@ -109,27 +218,25 @@ public class MainWindow implements ActionListener {
         JPanel southPanel = new JPanel();
         southPanel.setLayout(new FlowLayout());
 
-        JLabel labelPairs = new JLabel("Pairs Count: " + keyFigures.getCountPairs() +",");
-        JLabel labelSuccessors = new JLabel("Successors count: " + keyFigures.getCountSuccessors()+",");
-        JLabel labelDiversity = new JLabel("Gender Diversity Score: " + keyFigures.getGenderDiversityScore()+",");
+        JLabel labelPairs = new JLabel("Pairs Count: " + keyFigures.getCountPairs() + ",");
+        JLabel labelSuccessors = new JLabel("Successors count: " + keyFigures.getCountSuccessors() + ",");
+        JLabel labelDiversity = new JLabel("Gender Diversity Score: " + keyFigures.getGenderDiversityScore() + ",");
         JLabel labelAgeDifference = new JLabel("Age Difference: " + keyFigures.getAgeDifference());
-
 
         southPanel.add(labelPairs);
         southPanel.add(labelSuccessors);
         southPanel.add(labelDiversity);
         southPanel.add(labelAgeDifference);
 
-
         frame.add(southPanel, BorderLayout.SOUTH);
         frame.pack();
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
     }
+
     /**
      * Displays the table of groups with their relevant information and the pairs not in groups.
      */
-
     private void displayGroupTable() {
         DefaultTableModel model = new DefaultTableModel();
         GroupFactory GROUP_FACTORY = new GroupFactory(pairListFactory.pairList, PARTICIPANT_FACTORY.getPartyLocation());
@@ -261,6 +368,7 @@ public class MainWindow implements ActionListener {
         SET_CRITERIA.setText(bundle.getString("setCriteria"));
         START_PAIRS.setText(bundle.getString("startPairs"));
         START_GROUPS.setText(bundle.getString("startGroups"));
+        ADJUST_PAIRS.setText(bundle.getString("adjustPairs"));
         readParticipantsItem.setText(bundle.getString("readParticipants"));
         readParticipantsItem.setActionCommand(bundle.getString("readParticipants"));
         readPartyLocationItem.setText(bundle.getString("readPartyLocation"));
@@ -287,10 +395,10 @@ public class MainWindow implements ActionListener {
     }
 
     /**
-     * Will create a MenuBar using JMenuBar.
-     * @return a JMenuBar which could be used in the Main Window.
+     * Creates the menu bar for the main window.
+     *
+     * @return the created JMenuBar
      */
-
     private JMenuBar createJMenuBar() {
         JMenuBar menuBar = new JMenuBar();
 
@@ -299,13 +407,11 @@ public class MainWindow implements ActionListener {
         readParticipantsItem.addActionListener(this);
         startMenu.add(readParticipantsItem);
 
-
         readPartyLocationItem.setActionCommand(bundle.getString("readPartyLocation"));
         readPartyLocationItem.addActionListener(this);
         startMenu.add(readPartyLocationItem);
 
         menuBar.add(startMenu);
-
 
         START_PAIRS.addActionListener(this);
         START_PAIRS.setEnabled(criteriaOrdered);
@@ -314,6 +420,10 @@ public class MainWindow implements ActionListener {
         START_GROUPS.addActionListener(this);
         START_GROUPS.setEnabled(pairsGenerated);
         algorithmMenu.add(START_GROUPS);
+
+        ADJUST_PAIRS.addActionListener(this);
+        ADJUST_PAIRS.setEnabled(pairsGenerated);
+        algorithmMenu.add(ADJUST_PAIRS);
 
         SET_CRITERIA.addActionListener(this);
         SET_CRITERIA.setEnabled(participantsRead);
@@ -324,7 +434,6 @@ public class MainWindow implements ActionListener {
         startMenu.add(SHOW_PARTICIPANTS);
 
         menuBar.add(algorithmMenu);
-
 
         JMenuItem englishItem = new JMenuItem("English");
         englishItem.addActionListener(e -> loadLanguageResources("en"));
@@ -342,7 +451,7 @@ public class MainWindow implements ActionListener {
     }
 
     /**
-     * Will create a FileChooser using JFileChooser.
+     * Creates a file chooser dialog to select a CSV file.
      */
     private void createFileChooser() {
         JFileChooser fileChooser = new JFileChooser();
@@ -400,6 +509,8 @@ public class MainWindow implements ActionListener {
             displayPairTable();
         } else if (bundle.getString("startGroups").equals(command)) {
             displayGroupTable();
+        } else if (bundle.getString("adjustPairs").equals(command)) {
+            adjustPairs();
         }
 
         // Update menu items' text
@@ -407,11 +518,105 @@ public class MainWindow implements ActionListener {
         readPartyLocationItem.setText(bundle.getString("readPartyLocation"));
     }
 
+    /**
+     * Allows the user to manually adjust the pair list.
+     */
+    private void adjustPairs() {
+        DefaultTableModel model = new DefaultTableModel();
+        JTable table = new JTable(model);
+
+        model.addColumn(bundle.getString("pairNumber"));
+        model.addColumn(bundle.getString("participant1"));
+        model.addColumn(bundle.getString("participant2"));
+
+        for (int i = 0; i < pairListFactory.pairList.size(); i++) {
+            Pair pair = pairListFactory.pairList.get(i);
+            model.addRow(new Object[]{
+                    i + 1, // Pair number starts from 1
+                    pair.getParticipant1().getName(),
+                    pair.getParticipant2().getName()
+            });
+        }
+
+        JFrame frame = new JFrame(bundle.getString("adjustPairsTitle"));
+        frame.setLayout(new BorderLayout());
+        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+
+        JScrollPane tableScrollPane = new JScrollPane(table);
+        frame.add(tableScrollPane, BorderLayout.CENTER);
+
+        JPanel buttonPanel = new JPanel();
+        buttonPanel.setLayout(new FlowLayout());
+
+        JButton removeButton = new JButton(bundle.getString("removePairButton"));
+        removeButton.addActionListener(e -> removeSelectedPair(table));
+
+        JButton createPairButton = new JButton(bundle.getString("createPairButton"));
+        createPairButton.addActionListener(e -> createNewPair(table));
+
+        buttonPanel.add(removeButton);
+        buttonPanel.add(createPairButton);
+
+        frame.add(buttonPanel, BorderLayout.SOUTH);
+
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+    }
+
+    /**
+     * Removes the selected pair from the pair list.
+     *
+     * @param table the table displaying the pair list
+     */
+    private void removeSelectedPair(JTable table) {
+        int selectedRow = table.getSelectedRow();
+
+        if (selectedRow != -1) {
+            pairListFactory.pairList.remove(selectedRow);
+            ((DefaultTableModel) table.getModel()).removeRow(selectedRow);
+        }
+    }
+
+    /**
+     * Creates a new pair based on the selected participants from the table.
+     *
+     * @param table the table displaying the pair list
+     */
+    private void createNewPair(JTable table) {
+        int[] selectedRows = table.getSelectedRows();
+
+        if (selectedRows.length == 2) {
+            String participant1Name = (String) table.getValueAt(selectedRows[0], 1);
+            String participant2Name = (String) table.getValueAt(selectedRows[1], 1);
+
+            Participant participant1 = null;
+            Participant participant2 = null;
+
+            for (Participant participant : PARTICIPANT_FACTORY.getParticipantList()) {
+                if (participant.getName().equals(participant1Name)) {
+                    participant1 = participant;
+                } else if (participant.getName().equals(participant2Name)) {
+                    participant2 = participant;
+                }
+            }
+
+            if (participant1 != null && participant2 != null) {
+                Pair newPair = new Pair(participant1, participant2);
+                pairListFactory.pairList.add(newPair);
+                ((DefaultTableModel) table.getModel()).addRow(new Object[]{
+                        pairListFactory.pairList.size(), // Pair number is the size of the pair list
+                        newPair.getParticipant1().getName(),
+                        newPair.getParticipant2().getName()
+                });
+            }
+        }
+    }
 
 
 
     /**
-     * Will enable, disable submenus in the MenuBar.
+     * Updates the enabled status of submenus in the menu bar based on the current state.
      */
     public static void updateJMenu() {
         if (participantsRead) {
@@ -424,6 +629,7 @@ public class MainWindow implements ActionListener {
 
         if (criteriaOrdered) {
             START_PAIRS.setEnabled(true);
+            ADJUST_PAIRS.setEnabled(true);
         }
 
         if (pairsGenerated) {
@@ -446,5 +652,4 @@ public class MainWindow implements ActionListener {
     public static void setCriteriaOrdered(boolean isCriteriaOrdered) {
         criteriaOrdered = isCriteriaOrdered;
     }
-
 }
