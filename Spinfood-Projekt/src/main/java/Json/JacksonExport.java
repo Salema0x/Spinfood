c74406ddae70545e5d1d92ff11ebcdc796d501f8
Scheil <Scheil@students.uni-marburg.de>
2023-07-04 17:37:57 +0200

    refactored Looper -> GenerateSolutions and added Loesungen Directory

--- Spinfood-Projekt/src/main/java/Json/JacksonExport.java
+++ Spinfood-Projekt/src/main/java/Json/JacksonExport.java
@@ -50,7 +50,7 @@ public class JacksonExport {
     }
 
     /**
-     * generates a new Root Obj and then writes its Json-Repr into a file at given path
+     * generates a new Root Obj and then writes its Json-Repr into a new file at given path
      * @param groupList List containing all Groups
      * @param registeredPairsList List containing all registered Pairs
      * @param successorPairsList List containing all successor Pairs
@@ -59,8 +59,23 @@ public class JacksonExport {
      */
     public void exportToPath(List<Group> groupList, List<Pair> registeredPairsList, List<Pair> successorPairsList, List<Participant> successorParticipantsList,String filePath) {
         filePath = filePath;
-        export(groupList,registeredPairsList,successorPairsList,successorParticipantsList);
+        root = new Root(groupList, registeredPairsList, successorPairsList, successorParticipantsList);
+        jsonWriter();
         System.out.println("Groups saved at given Path: " + filePath);
+    }
+
+
+    /**
+     * generates a new Root Obj and then writes its Json-Repr into a given File
+     * @param groupList
+     * @param registeredPairsList
+     * @param successorPairsList
+     * @param successorParticipantsList
+     * @param file
+     */
+    public void exportToFile(List<Group> groupList, List<Pair> registeredPairsList, List<Pair> successorPairsList, List<Participant> successorParticipantsList, File file) {
+        root = new Root(groupList, registeredPairsList, successorPairsList, successorParticipantsList);
+        jsonFileWriter(file);
     }
 
     /**
@@ -69,6 +84,14 @@ public class JacksonExport {
     private void jsonWriter() {
         try {
             objectWriter.writeValue(new File(filePath), root);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void jsonFileWriter(File file) {
+        try {
+            objectWriter.writeValue(file, root);
         } catch (IOException e) {
             e.printStackTrace();
         }
